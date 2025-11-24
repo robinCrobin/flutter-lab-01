a656da2aa1254230239b9bb64436a5c991541e00
robertaSophia <roberta.silva@brius.com.br>
2025-11-24 11:20:54 -0300

    adicionando suporte ao plugin connectivity_plus nas plataformas macOS e Windows

--- task_manager/macos/Flutter/GeneratedPluginRegistrant.swift
+++ task_manager/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,6 +5,7 @@
 import FlutterMacOS
 import Foundation
 
+import connectivity_plus
 import device_info_plus
 import file_selector_macos
 import geolocator_apple
@@ -12,6 +13,7 @@ import path_provider_foundation
 import sqflite_darwin
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  ConnectivityPlugin.register(with: registry.registrar(forPlugin: "ConnectivityPlugin"))
   DeviceInfoPlusMacosPlugin.register(with: registry.registrar(forPlugin: "DeviceInfoPlusMacosPlugin"))
   FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
   GeolocatorPlugin.register(with: registry.registrar(forPlugin: "GeolocatorPlugin"))
